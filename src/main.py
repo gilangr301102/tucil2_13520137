250645e3001c1300bee7011573dd5d58a436d20f
gilangr301102 <muhgilangramadhan.3011@gmail.com>
2024-03-19 09:00:04 +0700

    init brute force and fix visualize data

--- src/main.py
+++ src/main.py
@@ -11,24 +11,34 @@ class Point:
 class CurveBezierSol():
     def __init__(self):
         # Initialize the input values
-        command = self.inputCommandHandling()
         self.initial_points = []
-        if(command=="2"):
-            file_content = self.fileInputHandling()
-
-            # Extract values from the input string
-            self.n = int(file_content[0])  # n is the first line
-            x_values = list(map(float, file_content[1].split()))
-            y_values = list(map(float, file_content[2].split()))
-            self.iteration = int(file_content[3])
-            for i in range(self.n):
-                self.initial_points.append(Point(x_values[i], y_values[i]))
-            # Print the extracted values
-            print("N:", self.n)
-            print("Points:", self.initial_points)
-            print("Iteration:", self.iteration)
-        else:
+        print("Silahkan Pilih Algoritma yang ingin dicoba: ")
+        print("1. Brute Force")
+        print("2. Divide and Conquer")
+        self.algoritma = str(input("Pilih Algoritma [1,2]: "))
+        if(self.algoritma=="1"):
+            print("Menggunakan Algoritma Brute Force")
             self.manualInputHandling()
+        else:
+            print("Menggunakan Algoritma Divide and Conquer")
+            command = self.inputCommandHandling()
+            if(command=="2"):
+                file_content = self.fileInputHandling()
+
+                # Extract values from the input string
+                self.n = int(file_content[0])  # n is the first line
+                x_values = list(map(float, file_content[1].split()))
+                y_values = list(map(float, file_content[2].split()))
+                self.iteration = int(file_content[3])
+                for i in range(self.n):
+                    self.initial_points.append(Point(x_values[i], y_values[i]))
+                # Print the extracted values
+                print("N:", self.n)
+                print("Points:", self.initial_points)
+                print("Iteration:", self.iteration)
+            else:
+                self.manualInputHandling()
+            self.is_visualize = str(input("Apakah ingin melihat proses visualisasi? [y/n]: "))
         self.bezier_generated_points = [[] for i in range(self.iteration+1)]
         self.bezier_curve_points = [[] for i in range(self.iteration+1)]
 
@@ -64,6 +74,15 @@ class CurveBezierSol():
     def get_midpoint(self, a: Point, b: Point):
         # Get the midpoint of two points
         return Point((a.x+b.x)/2,(a.y+b.y)/2)
+    
+    def generate_bezier_points_brute_force(self, initial_points, curr_iteration):
+        for i in range(curr_iteration):
+            temp_points = []
+            for j in range(len(initial_points)-1):
+                temp_points.append(self.get_midpoint(initial_points[j], initial_points[j+1]))
+            initial_points = temp_points
+        return initial_points
+
 
     def get_control_point(self, points: list[Point], curr_iteration, left_branches, right_branches):
         sz_points = len(points)
@@ -107,7 +126,7 @@ class CurveBezierSol():
         # Recursively left branches to get the next control points on the left
         self.generate_bezier_points_dnc(left_branches, curr_iteration)
 
-        # Append controll next control points on the bezier curve points
+        # Append next control points on the bezier curve points
         if(curr_iteration<=self.iteration):
             for i in range(curr_iteration, self.iteration+1):
                 self.bezier_curve_points[i].append(next_control_point)
@@ -116,16 +135,19 @@ class CurveBezierSol():
         self.generate_bezier_points_dnc(right_branches, curr_iteration)
 
     def get_bezier_solution(self):
-        # Append the initial points to the bezier curve points
-        for i in range(self.iteration+1):
-            self.bezier_curve_points[i].append(self.initial_points[0])
+        if(self.algoritma=="1"):
+            print(self.generate_bezier_points_brute_force())
+        else:
+            # Append the initial points to the bezier curve points
+            for i in range(self.iteration+1):
+                self.bezier_curve_points[i].append(self.initial_points[0])
 
-        # Generate the bezier points using divide and conquer
-        self.generate_bezier_points_dnc(self.initial_points, 0)
+            # Generate the bezier points using divide and conquer
+            self.generate_bezier_points_dnc(self.initial_points, 0)
 
-        # Append the last points to the bezier curve points
-        for i in range(self.iteration+1):
-            self.bezier_curve_points[i].append(self.initial_points[-1])
+            # Append the last points to the bezier curve points
+            for i in range(self.iteration+1):
+                self.bezier_curve_points[i].append(self.initial_points[-1])
 
     def visualize_all(self):
         # Visualize the bezier generated points and bezier curve points
@@ -133,23 +155,40 @@ class CurveBezierSol():
         visualize_points_x = []
         visualize_points_y = []
 
-        for i in range(self.iteration+1):
+        if(self.algoritma=="1"):
             temp_x = []
             temp_y = []
-            for j in range(len(self.bezier_generated_points[i])):
-                temp_x.append(self.bezier_generated_points[i][j].x)
-                temp_y.append(self.bezier_generated_points[i][j].y)
+            for j in range(len(self.bezier_curve_points[-1])):
+                temp_x.append(self.bezier_curve_points[-1][j].x)
+                temp_y.append(self.bezier_curve_points[-1][j].y)
             visualize_points_x.append(temp_x)
             visualize_points_y.append(temp_y)
-            temp_x = []
-            temp_y = []
-            for j in range(len(self.bezier_curve_points[i])):
-                temp_x.append(self.bezier_curve_points[i][j].x)
-                temp_y.append(self.bezier_curve_points[i][j].y)
-            visualize_points_x.append(temp_x)
-            visualize_points_y.append(temp_y)
-
-        # Initialize the plot
+        else:
+            if self.is_visualize == "y":
+                for i in range(self.iteration+1):
+                    temp_x = []
+                    temp_y = []
+                    for j in range(len(self.bezier_generated_points[i])):
+                        temp_x.append(self.bezier_generated_points[i][j].x)
+                        temp_y.append(self.bezier_generated_points[i][j].y)
+                    visualize_points_x.append(temp_x)
+                    visualize_points_y.append(temp_y)
+                    temp_x = []
+                    temp_y = []
+                    for j in range(len(self.bezier_curve_points[i])):
+                        temp_x.append(self.bezier_curve_points[i][j].x)
+                        temp_y.append(self.bezier_curve_points[i][j].y)
+                    visualize_points_x.append(temp_x)
+                    visualize_points_y.append(temp_y)
+            else:
+                temp_x = []
+                temp_y = []
+                for j in range(len(self.bezier_curve_points[-1])):
+                    temp_x.append(self.bezier_curve_points[-1][j].x)
+                    temp_y.append(self.bezier_curve_points[-1][j].y)
+                visualize_points_x.append(temp_x)
+                visualize_points_y.append(temp_y)
+                        # Initialize the plot
         fig, ax = plt.subplots()
         length_visualize_points = len(visualize_points_x)
         for i in range(length_visualize_points):
@@ -157,7 +196,7 @@ class CurveBezierSol():
             points, = ax.plot(visualize_points_x[i], visualize_points_y[i], 'o')
 
             # Randomize line and point colors
-            line_color = (i%2==1 and "#{:06x}".format(random.randint(0, 0xFFFFFF), label="Point") or "black")
+            line_color = ((i%2==1 or self.is_visualize=='n') and "#{:06x}".format(random.randint(0, 0xFFFFFF), label="Point") or "black")
             point_color = "#{:06x}".format(random.randint(0, 0xFFFFFF), label="Point")
 
             # Set line and point colors
@@ -168,7 +207,6 @@ class CurveBezierSol():
                 points.set_data(visualize_points_x[i][:j+1], visualize_points_y[i][:j+1])
                 plt.pause(0.4)
                 plt.draw()  # Update the plot
-
         # Adding labels and title
         ax.set_xlabel('X-axis')
         ax.set_ylabel('Y-axis')
